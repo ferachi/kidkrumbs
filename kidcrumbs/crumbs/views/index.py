674c7bf173434d3583d3c24c8236448ff36514d9
ferachi <gazaferachi@gmail.com>
2018-10-23 01:04:18 +0100

    Add splash screen
    - Added splash page
    - Modified Urls to place splash page as initial page
    - Added .swp files to gitignore

--- kidcrumbs/crumbs/views/index.py
+++ kidcrumbs/crumbs/views/index.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'crumbs/crumbs_base.html'
+
+class SplashView(TemplateView):
+    template_name = "crumbs/splash_screen.html"

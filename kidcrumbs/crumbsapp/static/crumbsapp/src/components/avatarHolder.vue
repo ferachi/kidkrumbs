723eeafbc3e12d6916e5d9093872c7ed707edc89
ferachi <gazaferachi@gmail.com>
2018-10-30 00:47:51 +0100

    Add user images placeholder component

---
+++ kidcrumbs/crumbsapp/static/crumbsapp/src/components/avatarHolder.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="avatar-holder">
+        <div v-if="image" class="image">
+            <img :src="image" :alt="alt" class="img-fluid" :class="{'rounded-circle':rounded}">
+        </div>
+        <div v-else class="image d-flex align-items-center justify-content-center" :class="{'grey_bg_2':showBg}">
+            <p class="grey_5" v-if="rounded"><i class="fas fa-user-circle fa-5x fa-fw"></i></p>
+            <p class="grey_5" v-else><i class="fas fa-user fa-5x fa-fw"></i></p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "",
+    mounted() {
+      let $image = $(".avatar-holder .image");
+      $image.css('height', $image.css('width'));
+  },
+    props:{
+        image:{
+            type:String,
+            default:''
+        },
+        alt:{
+            type:String,
+            default:''
+        },
+        rounded:{
+            type:Boolean,
+            default:false
+        },
+        showBg:{
+          type:Boolean,
+          default : true
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+</style>

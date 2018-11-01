64f4e6b0271606b9cd6b9ad2b8eabed4e1c62178
ferachi <gazaferachi@gmail.com>
2018-11-01 11:55:10 +0100

    Add tab components

--- kidcrumbs/crumbsapp/static/crumbsapp/src/components/Test.vue
+++ kidcrumbs/crumbsapp/static/crumbsapp/src/components/Test.vue
@@ -1,11 +1,54 @@
 <template>
     <div>
-        <h2>Not Found</h2>
+        <h4>TEST</h4>
+        <tabs justification="justify-content-end">
+            <tab name="menu" :selected="true"><div>
+    Consectetur totam aliquam aspernatur sequi fugiat dolorum? Debitis culpa ipsum magni repellendus nisi. Deleniti mollitia enim impedit dolores non Beatae dolorum corporis perferendis pariatur ab Sapiente sed consectetur aliquam autem
+</div>
+ </tab>
+            <tab name="plots">He must have used a lorem! </tab>
+        </tabs>
+        <div class="d-flex">
+        <p class="primary-color col-auto">Date</p>
+        <tab-head class="col" :tabTitles="['menu', 'home']" justify="justify-content-end" @tab-click="tabClicked($event)"></tab-head>
+        </div>
+        <div class="tabs">
+            <div v-if="selectedTab == 'menu'">
+                <h1>Menu</h1>
+            </div>
+            <div v-else-if="selectedTab == 'home'">
+                <h1>Home</h1>
+            </div>
+            <div v-else-if="selectedTab == 'away'">
+                <h1>Away</h1>
+            </div>
+            <div v-else></div>
+        </div>
+
+
     </div>
 </template>
 <script>
+import tab from "./tab.vue";
+import tabs from "./tabs.vue";
+import tabHead from "./tabHead.vue";
+
 export default {
     name : "Test",
+    data: () => ({
+        selectedTab : 'menu'
+    }),
+    components : {
+        tabs,
+        tab,
+        tabHead
+    },
+    methods:{
+        tabClicked(tab){
+            this.selectedTab = tab;
+        }
+    }
+
 }
 </script>
 
---
+++ kidcrumbs/crumbsapp/static/crumbsapp/src/components/tabHead.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="tab-head">
+        <div class="d-flex align-items-center" :class="justify">
+            <div v-for="tab in tabs" class="col-auto" @click="tabClicked(tab.name)">
+                <p class="text-center"><small class="color_3" :class="{'primary-color':tab.isActive}">{{tab.name}}</small></p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "",
+    created(){
+        this.tabs = this.tabTitles.map(title => ({ name : title, isActive:false}));
+        this.tabs[0].isActive = true;
+    },
+    props:{
+        tabTitles : {
+            type : Array,
+            required : true
+        },
+        justify:{
+            type:String,
+            default : "justify-content-center"
+        }
+    },
+    data(){
+        return {
+            tabs : []
+        }
+    },
+    methods : {
+        tabClicked(tab){
+            this.tabs.forEach(_tab => _tab.isActive = false);
+            let _tab = this.tabs.find(_tab => _tab.name == tab);
+            _tab.isActive = true;
+            this.$emit('tab-click', tab);
+        }
+    }
+};
+</script>
+<style lang="stylus">
+</style>

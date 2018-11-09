f627a42a61800429d8cb9c77f08590a70577ed5e
ferachi <gazaferachi@gmail.com>
2018-11-09 04:38:17 +0100

    Add prop to control the avatar size from parent components

---
+++ kidcrumbs/crumbsapp/static/crumbsapp/src/components/Comment.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="comment">
+        <div class="comments">
+            <div class="comment" v-for="chat in comments" :key="chat.id">
+                <div class="d-flex">
+                    <div class="col-xl-1 person-avatar">
+                        <avatar :showBg="false" :image="chat.person.avatar" :rounded="true"></avatar>
+                    </div>
+                    <div class="col">
+                        <div class="person-info">
+                            <h6 class="font-weight-bold">
+                                {{chat.person.fullname}} {{chat.date}} 
+                            </h6>
+                        </div>
+                        <div class="person-comment">
+                            {{chat.comment}}
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </div>
+</template>
+<script>
+import avatar from "./avatarHolder.vue";
+
+export default{
+    name : "Comment",
+    created(){
+    }, 
+    props:['comments'],
+    components:{
+        avatar
+    }
+}
+</script>
+<style lang="stylus">
+</style>
+
--- kidcrumbs/crumbsapp/static/crumbsapp/src/components/avatarHolder.vue
+++ kidcrumbs/crumbsapp/static/crumbsapp/src/components/avatarHolder.vue
@@ -4,8 +4,8 @@
             <img :src="image" :alt="alt" class="img-fluid" :class="{'rounded-circle':rounded}">
         </div>
         <div v-else class="image d-flex align-items-center justify-content-center" :class="{'grey_bg_2':showBg}">
-            <p class="grey_5" v-if="rounded"><i class="fas fa-user-circle fa-5x fa-fw"></i></p>
-            <p class="grey_5" v-else><i class="fas fa-user fa-5x fa-fw"></i></p>
+            <p class="grey_5" v-if="rounded"><i class="fas fa-user-circle fa-fw" :class="size"></i></p>
+            <p class="grey_5" v-else><i class="fas fa-user fa-fw" :class"size"></i></p>
         </div>
     </div>
 </template>
@@ -13,9 +13,9 @@
 export default {
     name: "",
     mounted() {
-      let $image = $(".avatar-holder .image");
-      $image.css('height', $image.css('width'));
-  },
+        let $image = $(".avatar-holder .image");
+        $image.css('height', $image.css('width'));
+    },
     props:{
         image:{
             type:String,
@@ -30,8 +30,12 @@ export default {
             default:false
         },
         showBg:{
-          type:Boolean,
-          default : true
+            type:Boolean,
+            default : true
+        },
+        size : {
+            type : String,
+            default : "fa-5x"
         }
     }
 }

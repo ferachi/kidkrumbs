3f41966f193068d92d970c24d58319a17dfd2f30
ferachi <gazaferachi@gmail.com>
2018-12-08 05:13:48 +0100

    Add page menu component to application
    The page menu component accompanies the page header component. i.e It is
    meant to be used with the page header (but not limited to this usage).
    The page menu component displays a component that is used for
    navigating within a page.

---
+++ kidcrumbs/crumbsapp/static/crumbsapp/src/components/pageMenu.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="pageMenu">
+        <div class="d-flex align-items-center py-2" :class="justify">
+            <div v-for="menu in menus" class="col-auto text-center px-2 menu" :class="[menu.active ? activeClasses : '']"
+                :key="menu.name" @click="menuClicked(menu.name)">
+                <slot :menu="menu" ></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "pageMenuComponent",
+    props:{
+        menus : {
+            type : Array, // array of objects with interface of {title, name, link, active, index}
+            required : true
+        },
+        justification:{
+            type : String,
+            default : 'between'
+        },
+        activeClasses : {
+            type : String,
+            default : 'active border-bottom primary-border'
+        }
+    },
+    computed : {
+        justify(){
+            let justifications = {
+                'center' : 'justify-content-center',  
+                'end' : 'justify-content-end',
+                'between' : 'justify-content-between',
+                'around' : 'justify-content-around',
+            }
+            return justifications[this.justification];
+        },
+    },
+    methods : {
+        menuClicked(selectedMenu){
+            // deactivate all menus
+            this.menus.forEach( _menu => _menu.active= false) ;
+
+            // get the selected menu
+            let _menu = this.menus.find( _menu => _menu.name == selectedMenu );    
+
+            // activate this menu
+            _menu.active = true;
+
+            
+            //emit it to parent
+            this.$emit('menu-click', _menu);
+        }
+    }
+};
+</script>
+<style lang="stylus">
+#pageMenu
+    .menu
+        transition border-color 0.3s ease-in-out
+        border-color transparent !important
+
+    .active
+        border-width 3px !important
+</style>
